35c0f4ca4cc3fc8d36eceb09f1644be0e8dd0e52
Laurynas Riliskis <laurynas.riliskis@ltu.se>
2011-08-18 13:38:03 +0000

    Copied code/symphony/trunk to code/symphony/branches/pre-3.11.
    git-svn-id: file:///home/svn/symphony/branches/pre-3.11@16 881c09cc-3da8-4082-bb0d-d8ce5b87ff19

---
+++ tos_ns/src/symphony/tos-bridge/TinyBridge.h
@@ -0,0 +1,94 @@
+/*
+ * TinyBridge.h
+ *
+ *  Created on: Apr 27, 2010
+ *      Author: laurynas
+ */
+#ifndef __USE_GNU
+#define __USE_GNU
+#endif
+
+#ifndef TINYBRIDGE_H_
+#define TINYBRIDGE_H_
+
+#include "LibToTosProxy.h"
+#include "TosToLibProxy.h"
+#include "LibToTosProxy.h"
+#include "ns3/TosNode.h"
+#include "ns3/SimuClock.h"
+
+
+/**
+ * cast type of functions from ns to tos
+ */
+typedef int(LibToTosProxy::*fucnpoint)(int);
+
+class TinyBridge {
+public:
+	TinyBridge(ns3::TosNode * tos, const char * lib);
+	/**
+	 * Returns pointer to tos library function
+	 * For now its predefined
+	 */
+	void * getFunc(const char *);
+	
+	/**
+	 * Destructor of the bridge class
+	 * 
+	 */
+    ~TinyBridge();
+    
+private:
+    /**
+     * Function that opens library
+     * and bridges objects and lib together
+     */
+    void bridgeObjects(void);
+    
+    /**
+     * library handler
+     */
+    void *handler;   
+    
+    /**
+     * Unique id of this
+     */
+    int un;
+
+    /**
+     * Function to set proxy in node image
+     */
+    tosfunc setObj;
+
+    tosfunc run_next;
+    /**
+     * name of the library to open
+     */
+    const char * libname;
+    /**
+     * Error variable, used then opening library
+     */
+    char *error;
+    /**
+     * Pointer to TosNode class
+     */
+    ns3::TosNode *tosnode;
+    /**
+     * Pointer to proxy for library to tos node communications
+     */
+    LibToTosProxy *libtotos;
+    /**
+     * Pointer to orxy for tos to lib communiactions
+     */
+    TosToLibProxy *tostolib;
+
+    /**
+     * Node clock
+     */
+    //SimuClock * clock;
+
+
+    //LibToTosPointers *proxy;
+};
+
+#endif /* TINYBRIDGE_H_ */
---
+++ tos_ns/src/symphony/tos-radio/symphony.h
@@ -0,0 +1,23 @@
+#ifndef SYMPHONY_H
+#define SYMPHONY_H
+
+
+
+namespace ns3 {
+
+class Symphony
+{
+public:
+    Symphony();
+    virtual ~Symphony ();
+    
+private:
+    int id;
+
+};
+
+
+
+} // namespace ns3
+
+#endif /* SYMPHONY_H */
